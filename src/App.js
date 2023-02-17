9b021a3afb543990108061954e3ca6c04cbee70c
mezotamas <jakubatzita@gmail.com>
2023-02-17 08:09:41 +0100

    Fölösleges menük törlése

--- src/App.js
+++ src/App.js
@@ -60,18 +60,17 @@ class App extends Component {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Navbar.Brand href="#home">
         
-        Dice Roller
+        Mit Üzen Neked?
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-        <Link to={"/"} className="navbar-brand">
-            bezKoder
-          </Link>
+        
           <div className="navbar-nav mr-auto">
+            
             <li className="nav-item">
-              <Link to={"/home"} className="nav-link">
-                Home
+              <Link to={"/Kezdolap"} className="nav-link">
+                Kezdőlap
               </Link>
             </li>
             <li className="nav-item">
@@ -89,19 +88,9 @@ class App extends Component {
                 Felvitel
               </Link>
             </li>
-            <li className="nav-item">
-              <Link to={"/Kezdolap"} className="nav-link">
-                Kezdőlap
-              </Link>
-            </li>
+            
 
-            {showModeratorBoard && (
-              <li className="nav-item">
-                <Link to={"/mod"} className="nav-link">
-                  Moderator Board
-                </Link>
-              </li>
-            )}
+            
 
             {showAdminBoard && (
               <li className="nav-item">
@@ -124,27 +113,9 @@ class App extends Component {
                 </Link>
               </li>
 
-            {currentUser && (
-              <li className="nav-item">
-                <Link to={"/user"} className="nav-link">
-                  User
-                </Link>
-              </li>
-            )}
+            
           </div>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
+      
         </Nav>
         <Nav>
         {currentUser ? (

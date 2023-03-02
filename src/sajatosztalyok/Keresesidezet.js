e20c9bdfaa4b4daca824d604427261b837885f0a
mezotamas <114439916+mezotamas@users.noreply.github.com>
2023-03-02 09:42:53 +0100

    ...

--- src/sajatosztalyok/Keresesidezet.js
+++ src/sajatosztalyok/Keresesidezet.js
@@ -54,7 +54,7 @@ export default class FetchExample extends React.Component {
 
   keres=()=>{
       //alert("Hello")
-      if(this.state.bevitel1=="" || this.state.bevitel2==""||this.state.bevitel3=="")
+      if(this.state.bevitel1=="" )
       alert("Nem maradhat üresen!")
       else{
 
